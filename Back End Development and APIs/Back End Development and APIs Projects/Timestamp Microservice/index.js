a548915440197bdc3e38cf30d1a04cddd1c2db22
Ernest <iendeavor.github.io@gmail.com>
2024-04-12 15:59:14 +0800

    implement Timestamp Microservice

--- Back End Development and APIs/Back End Development and APIs Projects/Timestamp Microservice/index.js
+++ Back End Development and APIs/Back End Development and APIs Projects/Timestamp Microservice/index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,10 +19,23 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get('/api/:date?', function(req, res) {
+  const timestamp = req.params.date
+    ? /^\d+$/.test(req.params.date)
+      ? parseInt(req.params.date)
+      : new Date(req.params.date).getTime()
+    : Date.now()
+  if (isNaN(new Date(timestamp))) {
+    res.json({ error: 'Invalid Date' })
+  } else {
+    res.json({ unix: timestamp, utc: new Date(timestamp).toGMTString() })
+  }
+})
 
 
 
