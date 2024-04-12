e0a07979dc459783253b4ef1f2670212dcba7a7c
Ernest <iendeavor.github.io@gmail.com>
2024-04-12 16:17:42 +0800

    implement URL Shortener Microservice

--- Back End Development and APIs/Back End Development and APIs Projects/URL Shortener Microservice/index.js
+++ Back End Development and APIs/Back End Development and APIs Projects/URL Shortener Microservice/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
@@ -18,6 +21,28 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const shortUrlMap = []
+
+app.get('/api/shorturl/:index', function(req, res) {
+  if (shortUrlMap[req.params.index]) {
+    res.redirect(shortUrlMap[req.params.index])
+  } else {
+    res.json({ error: "Invalid URL" })
+  }
+});
+
+app.post('/api/shorturl', function(req, res) {
+  try {
+    const original_url = new URL(req.body.url)
+    if (original_url.protocol !== 'http:') throw Error()
+    const short_url = (shortUrlMap.push(original_url) - 1).toString()
+    res.json({ original_url: original_url.toString(), short_url });
+  } catch {
+    res.json({ error: "Invalid URL" })
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
